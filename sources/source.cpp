d86e480424ea00343c30e3eb622c0c2e20ba0d5d
Sergei2000 <dsdem@mail.ru>
2020-03-15 20:12:49 +0300

    last code change

--- sources/source.cpp
+++ sources/source.cpp
@@ -2,11 +2,12 @@
 
 #include <header.hpp>
 #include <iostream>
-#include<boost/asio.hpp>
+#include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
+#include <boost/log/trivial.hpp>
 #include <boost/thread/recursive_mutex.hpp>
 #include <system_error>
-#include <unistd.h>
+
 
 static const int buf_size =500;
 static boost::thread_group threads;
@@ -18,60 +19,104 @@ static std::vector<socket_ptr> sockets;
 static boost::mutex mutex1,mutex_socket,mutex_clients;
 static boost::recursive_mutex mutex;
 static std::vector<int> num_close;
-void clear_buf(char * buf,int size)
-{
-    for (int i=0;i<size;++i)
-    {
-        buf[i]='/0';
-    }
-}
+static std::string client_list="client list: ";
+boost::recursive_mutex cs,ds;
+
 
 struct client
 {
     ip::tcp::socket _sock{service};
     bool _status=true;
-    void write (std:: string msg)
+    bool _initial = true;
+    char buf[200] ;
+    void clear_buf(char * buf,int size)
     {
-        _sock.write_some(boost::asio::buffer(msg,7));
+        for (int i=0;i<size;++i)
+        {
+            buf[i]='/0';
+        }
     }
-    std::string _reply;
+    void write (std::string msg)
+    {
+        int a=msg.size();
+       _sock.write_some(boost::asio::buffer(&a,4));
+        _sock.write_some(boost::asio::buffer(msg,a));
+    }
+    std::string _reply,_name;
     void read_str()
     {
         if (_sock.available())
         {
-            char buf[200] ;
-            int bytes=_sock.read_some(boost::asio::buffer(buf,200));
-            for (int i=0;i<bytes;++i)
-            {
-                _reply[i]=std::move(buf[i]);
-            }
+            int counter;
+            clear_buf(buf,40);
+            _sock.read_some(boost::asio::buffer(&counter,4));
+            _sock.read_some(boost::asio::buffer(buf,counter));
+            _reply=std::string(buf);
+            _reply=_reply.substr(0,_reply.find('/0'));
+            //std::cout<< _reply;
+            clear_buf(buf,40);
         }
 
     }
     void ping()
     {
-        write("ping_ok");
+        write("ping_ok\n");
     }
     void communicate()
     {
         try
         {
-            ping();
+            alarm(10);
+            if (_initial) {
+                read_str();
+                boost::recursive_mutex::scoped_lock lk(ds);
+                _name = _reply;
+                if (_name.find("ping\n") != std::string::npos)
+                {
+                    _name=_name.substr(0,_name.find("ping\n"));
+                }
+                if (_name.find("client list\n") != std::string::npos)
+                {
+                    _name=_name.substr(0,_name.find("client list\n"));
+                }
+                if (client_list.find(_name)==std::string::npos)
+                {
+                    client_list+=(" "+_name+" ");
+                }
+                BOOST_LOG_TRIVIAL(info) <<_name;
+                _initial=false;
+            }
+            clear_buf(buf,40);
             read_str();
-            std::cout<< _reply;
+           // std::cout<<_reply;
+            if (_reply=="ping\n")
+            {
+                ping();
+            } else
+            {
+                if (_reply.find("client list\n")!=std::string::npos)
+                {
+                    //std::cout<<_reply<<std::endl;
+                    boost::recursive_mutex::scoped_lock lk(ds);
+                    write(client_list);
+                }
+
+            }
+          alarm(0);
         }
-        catch ( boost::system::system_error&)
+        catch ( ... )
         {
             if (_sock.available())
             {
                 _sock.close();
             }
             _status=false;
-            std::cout<<"no client";
-
+            fflush(stdout);
+            //std::cout<<"no client"+_name;
+            client_list.erase(client_list.find(_name),_name.size());
+            BOOST_LOG_TRIVIAL(error) <<_name;
+            alarm(0);
         }
-
-
     }
     bool disconnected()
     {
@@ -84,7 +129,8 @@ struct client
 
 typedef boost::shared_ptr<client> client_ptr;
 static std::vector<client_ptr> clients;
-boost::recursive_mutex cs;
+
+
 
 
 void communication_with_server()
@@ -114,7 +160,6 @@ void communication_with_server()
 
 void access_func()
 {
-
     while(true)
     {
         client_ptr one(new client);
@@ -126,7 +171,6 @@ void access_func()
 
 int main(int argc, char* argv[])
 {
-
     boost::thread first(access_func);
     boost::thread second(communication_with_server);
     first.join();
